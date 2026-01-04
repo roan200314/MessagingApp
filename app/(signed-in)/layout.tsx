957fea1c39ca23d7a49e82e27625f3fe948bda43
hellyr <hellyr@hva.nl>
2026-01-05 00:26:09 +0100

    making dashboard ui

--- app/(signed-in)/layout.tsx
+++ app/(signed-in)/layout.tsx
@@ -1,11 +1,50 @@
 "use client"
 
+import { Chat } from "stream-chat-react";
 import UserSyncWrapper from "@/components/UserSyncWrapper";
+import streamClient from "@/lib/stream";
+import { 
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { Separator } from "@/components/ui/separator";
+import { Link } from "lucide-react";
+
+
 
 function Layout( {children}: { children: React.ReactNode }) {
-    return (
-        <UserSyncWrapper>{children}</UserSyncWrapper>
-    )
+    return <UserSyncWrapper>
+        <Chat client={streamClient}>
+                <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "19rem",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <Link href="/dashboard">
+          <h1 className="text-lg font-bold tracking-wider uppercase">
+            ToConnect
+            </h1>
+          </Link>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+        </Chat>
+        </UserSyncWrapper>
 }
 
 export default Layout;
